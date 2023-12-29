9bd05473adc31fca609e5fc46c4c2aa47e811a46
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-29 19:15:07 +0530

    Create 1_introToSystemdesign.cpp

---
+++ 1_introToSystemdesign.cpp
@@ -0,0 +1,14 @@
+// You have pizza parlour
+// how to optimise it??
+// -> optimise process and increase output in same resources (vertical scaling)
+// -> preparing beforehand during non-peak hours (preprocessing)
+// -> backup chef( backup server)
+// -> hire more chef (horizontal scaling)
+// -> alot task to specialse chefs( miscroservices)
+// -> different shops( distributed system)
+// -> minimize the quene time for customer (load balancing)
+// -> Decoupling
+// -> notes every task (logging and metrics calculations)
+// -> extensible
+
+// HLD vs LLD
