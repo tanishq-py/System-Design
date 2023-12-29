d4c2bdc5328c0de2b58f658853a7410c2ba32ef4
Tanishq Parihar <tanishqparihar3@gmail.com>
2023-12-29 20:41:26 +0530

    Create HorizontalVSVertical.cpp

---
+++ HorizontalVSVertical.cpp
@@ -0,0 +1,12 @@
+// request->API->response
+// buy bigger machine (vertical scaling)
+// but more machines (horizontal scaling)
+
+// horizontal -
+// ->load balancing req.
+// -> resillient
+// -> network calls
+// -> data inconsistency
+
+
+  
